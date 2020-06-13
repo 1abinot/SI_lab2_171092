a051e407c1c10b5b2df7a97b7141522d34ce1716
Labinot Aziri <labi.aziri1@gmail.com>
2020-06-13 03:36:41 +0200

    Multiple Condition Test

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -32,4 +32,41 @@ class SILab2Test {
         //A,B - C ; C - D,E ; D,E - F ; F - G ; G - H ; H - I ; I - J ; J - K ; K - L ; L - M ; M - N ; N - O ; O - P
         assertEquals(true, obj.function(new User("labinot", "Labi.1234", "labi.aziri1@gmail.com"), addUserToList("labinot", "Labinot.123", "labi.aziri1@gmail.com")));
     }
+
+    @Test
+    void multipleConditionsTest() {
+
+        //user.getUsername()!=null && user.getPassword()!=null
+
+        //T T
+        assertEquals(true, obj.function(new User("labinot","Labi.1234","labi.aziri1@gmail.com"),addUserToList("labinot","labinot","labi.aziri1@gmail.com")));
+        //T F
+        assertEquals(false, obj.function(new User("labinot", "labi", "labi.aziri1@gmail.com"), addUserToList("labinot", "labi", "labi.aziri1@gmail.com")));
+        //F X
+        assertEquals(false,obj.function(new User("labinot","labi.123","labi.aziri1@gmail.com"),addUserToList("labinot","labi.123","labi.aziri1@gmail.com")));
+
+
+
+        //!passwordLower.contains(user.getUsername().toLowerCase()) && password.length()>=8
+
+        //T T
+        assertEquals(true, obj.function(new User("labinot","Labi_123","labi.aziri1@gmail.com"),addUserToList("labinot","Labi_1234","labi.aziri1@gmail.com")));
+        //T F
+        assertEquals(false, obj.function(new User("labinot","Labi123","labi.aziri1@gmail.com"),addUserToList("labinot","Labi_123","labi.aziri1@gmail.com")));
+        //F X
+        assertEquals(false, obj.function(new User("labinot", "labinot", "labi.aziri1@gmail.com"), addUserToList("labinot", "labinot", "labi.aziri1@gmail.com")));
+
+
+        //digit && upper && special
+
+        //T T T
+        assertEquals(true, obj.function(new User("labinot", "Labi_1234", "labi.aziri1@gmail.com"), addUserToList("labinot", "Labi_1234", "labi.aziri1@gmail.com")));
+        //T T F
+        assertEquals(false, obj.function(new User("labinot", "Labi1234", "labi.aziri1@gmail.com"), addUserToList("labinot", "Labi1234", "labi.aziri1@gmail.com")));
+        //T F X
+        assertEquals(false, obj.function(new User("labinot", "labi_1234", "labi.aziri1@gmail.com"), addUserToList("labinot", "labi_1234", "labi.aziri1@gmail.com")));
+        //F X X
+        assertEquals(false, obj.function(new User("labinot", "labinot_", "labi.aziri1@gmail.com"), addUserToList("labinot", "labinot_", "labi.aziri1@gmail.com")));
+    }
+
 }
